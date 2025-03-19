e1058a6c9ef6598d42cd1a0e428e5eb87f5b5eff
Abramo Franchetti <abramo.franchetti@sanmarcoinformatica.it>
2025-03-19 11:23:41 +0100

    fix

--- js/cppa.js
+++ js/cppa.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const yearsSlider = document.getElementById('yearsSlider');
     const yearsValueSpan = document.getElementById('yearsValue');
     const resultText = document.getElementById('resultText');
+    const reverseResultText = document.getElementById('reverseResultText');
 
     function formatEuro(value) {
         return new Intl.NumberFormat('it-IT', {
@@ -18,16 +19,19 @@ document.addEventListener('DOMContentLoaded', function () {
     function calculateInflationLoss(initialCapital, inflationRate, years) {
         const cumulativeFactor = Math.pow(1 + inflationRate / 100, years);
         const svalutatedValue = Math.round(initialCapital / cumulativeFactor);
+        const rivalutatedValue = Math.round(initialCapital * cumulativeFactor);
         const lossPercentage = (((svalutatedValue / initialCapital) - 1) * -100).toFixed(2);
-        return { svalutatedValue, lossPercentage };
+        const gainPercentage = (((rivalutatedValue / initialCapital) - 1) * 100).toFixed(2);
+        return { svalutatedValue, rivalutatedValue, lossPercentage, gainPercentage };
     }
 
     function updateResult() {
         const initialCapital = parseFloat(initialCapitalInput.value);
         const inflationRate = parseFloat(inflationSlider.value);
         const years = parseInt(yearsSlider.value);
-        const { svalutatedValue, lossPercentage } = calculateInflationLoss(initialCapital, inflationRate, years);
-        resultText.innerText = `Il potere di acquisto del capitale iniziale di ${formatEuro(initialCapital)} per via dell'inflazione cumulata del ${inflationRate}% in ${years} anni è diventato ${formatEuro(svalutatedValue)} con una perdita di potere di acquisto del ${lossPercentage}%`;
+        const { svalutatedValue, rivalutatedValue, lossPercentage, gainPercentage } = calculateInflationLoss(initialCapital, inflationRate, years);
+        resultText.innerText = `Il potere di acquisto del capitale iniziale di ${formatEuro(initialCapital)} per via dell'inflazione cumulata del ${inflationRate}% in ${years} anni è diventato ${formatEuro(svalutatedValue)} con una perdita di potere di acquisto del ${lossPercentage}%.`;
+        reverseResultText.innerText = `Al contrario, per acquistare ciò che avresti acquistato ${years} anni fa con il capitale iniziale di ${formatEuro(initialCapital)}, ora ti servirebbero ${formatEuro(rivalutatedValue)} rivalutati, con un esborso maggiorato del ${gainPercentage}%.`;
     }
 
     initialCapitalInput.addEventListener('input', updateResult);
